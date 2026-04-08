5567504f94fbd475af55e33ddd1cb02801a4a501
KraftKreator <158065122+KraftKreator@users.noreply.github.com>
2026-04-09 00:23:26 +0200

    Add files via upload

---
+++ piotrsiudeja-main/script.js
@@ -0,0 +1,8 @@
+
+// burger service menu
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu-wrapper');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
