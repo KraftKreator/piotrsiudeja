a8e44420e0d140e57062c18c1608834652875462
KraftKreator <158065122+KraftKreator@users.noreply.github.com>
2026-05-06 14:19:31 +0200

    logotype-design.html updates for development

--- piotrsiudeja-main/README.md
+++
@@ -1,2 +0,0 @@
-# piotrsiudeja
-designer
--- piotrsiudeja-main/script.js
+++
@@ -1,8 +0,0 @@
-
-// burger service menu
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.menu-wrapper');
-
-burger.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
